44595bb53cbcb251ac40e1ef2f472beed7350502
null0ptr <nazshak6@gmail.com>
2024-02-20 20:17:29 +0600

    add server connection feature

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+import { config } from 'dotenv'
+import express from 'express'
+
+config()
+const app = express()
+
+app.listen(process.env.PORT, () => {
+	console.log(`listening on port ${process.env.PORT}`)
+})
